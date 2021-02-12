9d5666e7fff63a5dcaa32719ccb802441ff021ef
ZR Saimun <zrsaimun@gmail.com>
2021-02-13 02:40:57 +0600

    Created 2nd page

---
+++ src/resource.js
@@ -0,0 +1,13 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+
+    frogNormal_png : "res/frog00.png",
+    CloseN_png : "res/CloseN.png",
+    CloseS_png : "res/CloseS.png",
+    Main_Music : "res/sound/mainmusic.mp3"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
